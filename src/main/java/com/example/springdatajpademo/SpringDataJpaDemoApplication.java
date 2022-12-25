70cb58c0512c12c267743697e21386cd0c1392c0
amit <amit.gunner73@gmail.com>
2022-12-26 02:57:49 +0600

    Implement Spring jdbc

---
+++ src/main/java/com/example/springdatajpademo/SpringDataJpaDemoApplication.java
@@ -0,0 +1,25 @@
+package com.example.springdatajpademo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaDemoApplication implements CommandLineRunner {
+
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+	@Autowired
+	PersonJdbcDao dao;
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaDemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info("All user -> {}", dao.findAll());
+	}
+}
